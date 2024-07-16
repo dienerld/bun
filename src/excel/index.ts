127001b08e59458752f702870eac948ba440d8bf
Diener Dornelas <diener.ld@outlook.com>
2024-07-16 10:36:13 -0300

    refactor: ajustes exceljs

--- src/excel/index.ts
+++ src/excel/index.ts
@@ -1,89 +1,127 @@
-import ExcelJS from 'exceljs'
+import ExcelJS from "exceljs";
+import { mapperColumns } from "./mapper-columns";
 
-const workbook = new ExcelJS.Workbook()
+const workbook = new ExcelJS.Workbook();
 // create spreadsheet
-const sheet = workbook.addWorksheet('Sheet1', {
-  views: [{ showGridLines: true }]
+const sheet = workbook.addWorksheet("Sheet1", {
+  views: [{ showGridLines: true, style: 'pageLayout', }],
+
+});
+
+const columns: Partial<ExcelJS.Column>[] = [
+  {
+    header: "Name", key: "name",
+    style: {
+      font: { bold: true, size: 12, color: { argb: "FF000000" } },
+    },
+  },
+  {
+    header: "Idade", key: "age", style: {
+      font: { bold: true, size: 12, color: { argb: "FF000000" } },
+    },
+  },
+  {
+    header: "Sexo", key: 'gender', style: {
+      font: { bold: true, size: 12, color: { argb: "FF000000" } },
+    },
+  },
+  {
+    header: "Endereço", key: "address", style: {
+      font: { bold: true, size: 12, color: { argb: "FF000000" } },
+    },
+  },
+  {
+    header: "latitude", key: "lat", style: {
+      numFmt: "0.000000", // melhorar a formatação
+      font: { bold: true, size: 12, color: { argb: "FF000000" } },
+    },
+  },
+  {
+    header: "longitude", key: "lng", style: {
+      numFmt: "0.000000",
+      font: { bold: true, size: 12, color: { argb: "FF000000" } },
+    },
+  },
+];
+sheet.columns = columns;
+
+
+sheet.insertRow(1, ["Relatório de posições - Placa: RYF8I86 | ID_Disp: 840083328 | Rótulo: MOTORISTA SERJÃO"]).eachCell((cell) => {
+  cell.font = { bold: true, size: 24, color: { argb: 'FF000000' } };
+  cell.alignment = { vertical: "middle", horizontal: "center" };
 })
+sheet.mergeCells(`A1:${mapperColumns(columns.length)}1`);
 
 
 function AdjustColumnWidth(worksheet: ExcelJS.Worksheet) {
-  worksheet.columns.forEach(column => {
+
+  worksheet.columns.forEach((column) => {
     if (!column.values) return;
-    const lengths = column.values.map(v => v ? v.toString().length : 0);
-    const maxLength = Math.max(...lengths.filter(v => typeof v === 'number'));
+    // `i` Forma para ignorar o registro do header e não usa-lo como referencia para o calculo do tamanho da coluna
+    const lengths = column.values.map((v, i) => (v && i > 2 ? v.toString().length : 0));
+    const maxLength = Math.max(...lengths.filter((v) => typeof v === "number"));
     const headerLength = column.header ? column.header.length : 0;
 
-    column.width = Math.max(maxLength, headerLength) + 2;
+    column.width = Math.max(maxLength, headerLength) + 6;
   });
 }
 
-// // create header row with backgorund color
-// sheet.addRow(['Name', 'Idade', 'Sexo', 'endereco']).eachCell((cell) => {
-//   cell.fill = {
-//     type: 'pattern',
-//     pattern: 'solid',
-//     fgColor: { argb: 'FF0000' },
-//   }
-// })
-sheet.columns = [
-  { header: 'Name', key: 'name' },
-  { header: 'Idade', key: 'age' },
-  { header: 'sexo', key: 'genre' },
-  { header: 'endereco', key: 'address', },
-]
-
-sheet.eachRow((row) => {
-  row.eachCell((cell) => {
-    cell.style = {
-      alignment: { vertical: 'middle', horizontal: 'center' },
-      font: { bold: true },
-      fill: {
-        type: 'pattern',
-        pattern: 'solid',
-        fgColor: { argb: 'FF0000' },
-      },
-      border: {
-        top: { style: 'thin' },
-        left: { style: 'thin' },
-        bottom: { style: 'thin' },
-        right: { style: 'thin' },
-      }
-    }
-  })
-})
-
-const rows: [][] = [
-  ['John Doe', 30, 'M', 'COM GPS - R. PORTO VELHO, 1119 - INDUSTRIAL, LUCAS DO RIO VERDE - MT, 78455-000'],
-  ['Jane Doe', 25, 'F', 'COM GPS - R. PORTO VELHO, 1119 - INDUSTRIAL, LUCAS DO RIO VERDE - MT, 78455-000'],
-  ['John Smith', 40, 'M', 'COM GPS - R. PORTO VELHO, 1119 - INDUSTRIAL, LUCAS DO RIO VERDE - MT, 78455-000'],
-  ['Jane Smith', 35, 'F', 'COM GPS - R. PORTO VELHO, 1119 - INDUSTRIAL, LUCAS DO RIO VERDE - MT, 78455-000'],
-]
+const rows: any[][] = [
+  [
+    "John Doe",
+    25,
+    "M",
+    "COM GPS - R. PORTO VELHO, 1119 - INDUSTRIAL, LUCAS DO RIO VERDE - MT, 78455-000",
+    -12.135847,
+    -58.008802
+  ],
+  [
+    "Jane Doe",
+    25,
+    "F",
+    "COM GPS - R. PORTO VELHO, 1119 - INDUSTRIAL, LUCAS DO RIO VERDE - MT, 78455-000",
+    -12.135847,
+    -58.008802
+  ],
+  [
+    "John Smith",
+    40,
+    "M",
+    "COM GPS - R. PORTO VELHO, 1119 - INDUSTRIAL, LUCAS DO RIO VERDE - MT, 78455-000",
+  ],
+  [
+    "Jane Smith",
+    35,
+    "F",
+    "COM GPS - R. PORTO VELHO, 1119 - INDUSTRIAL, LUCAS DO RIO VERDE - MT, 78455-000",
+    -12.135847,
+    -58.008802
+  ],
+];
 
 // add rows striped
 
 rows.forEach((row, index) => {
-  const rowNumber = index + 2
   sheet.addRow(row).eachCell((cell) => {
     cell.style = {
-      alignment: { vertical: 'middle', horizontal: 'left' },
+      alignment: { vertical: "middle", horizontal: "left" },
       fill: {
-        type: 'pattern',
-        pattern: 'solid',
-        fgColor: { argb: index % 2 === 0 ? 'FFFFFF' : 'DDDDDD' },
+        type: "pattern",
+        pattern: "solid",
+        fgColor: { argb: index % 2 === 0 ? "FFFFFFFF" : "FFDDDDDD" },
       },
       border: {
-        top: { style: 'thin' },
-        left: { style: 'thin' },
-        bottom: { style: 'thin' },
-        right: { style: 'thin' },
-      }
-    }
-  })
-})
+        top: { style: "thin" },
+        left: { style: "thin" },
+        bottom: { style: "thin" },
+        right: { style: "thin" },
+      },
+    };
+  });
+});
 
-AdjustColumnWidth(sheet)
+AdjustColumnWidth(sheet);
 // save file
-workbook.xlsx.writeFile('example.xlsx').then(() => {
-  console.log('File saved!')
-})
+workbook.xlsx.writeFile("example.xlsx").then(() => {
+  console.log("File saved!");
+});
---
+++ src/excel/mapper-columns.ts
@@ -0,0 +1,5 @@
+type CharAlphabet = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z";
+export const mapperColumns = (lengthColumns: number): CharAlphabet => {
+  const alphabet: CharAlphabet[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  return alphabet[lengthColumns - 1] as CharAlphabet;
+}
